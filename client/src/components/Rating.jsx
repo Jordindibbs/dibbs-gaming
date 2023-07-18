00b2567e4aeba627a20921c726456e92c202215b
Jordindibbs <jordindibbs@gmail.com>
2023-07-17 19:01:09 -0700

    Fixed Names
    Had an issue where github lowercased some files.

---
+++ client/src/components/Rating.jsx
@@ -0,0 +1,60 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className="rating">
+      <span>
+        {value >= 2 ? (
+          <FaStar />
+        ) : value >= 0.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 4 ? (
+          <FaStar />
+        ) : value >= 2.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 7 ? (
+          <FaStar />
+        ) : value >= 4.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 9 ? (
+          <FaStar />
+        ) : value >= 7.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 10 ? (
+          <FaStar />
+        ) : value >= 9.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span className="rating-text">{text && text}</span>
+    </div>
+  );
+};
+
+Rating.defaultProps = {
+  color: "#f8e825",
+};
+
+export default Rating;
